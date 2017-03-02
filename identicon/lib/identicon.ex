49997cf201bc5e60d4de7497459a1c46814f8065
javier <4.1.4.1.done@gmail.com>
2017-03-02 06:42:53 -0800

    image gen prep done

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -3,6 +3,8 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
+    |> filter_odd_squares
   end
 
   def hash_input(input) do
@@ -12,12 +14,36 @@ defmodule Identicon do
     %Identicon.Image{hex: hex}
   end
 
-  # first three values can be rgb
-  # Can pattern match in arg list
+
   def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
     # Changed to a tuple since each has meaning
     %Identicon.Image{image | color: {r, g, b}}
   end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    grid =
+      hex
+      |> Enum.chunk(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index # creates a tuple with the second element as the index
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid = Enum.filter grid, fn({code, _index}) ->
+      rem(code, 2) == 0
+    end
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def mirror_row(row) do
+    [first, second | _tail] = row
+    row ++ [second, first]
+  end
+
 
   # Color on even numbers, don't color on odd
 end

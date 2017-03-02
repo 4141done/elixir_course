0d57c7affb62d89e7b8483039f3ace4bc145d123
javier <4.1.4.1.done@gmail.com>
2017-03-02 06:22:47 -0800

    updated strucuts

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -2,10 +2,23 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_input
+    |> pick_color
   end
 
   def hash_input(input) do
-    :crypto.hash(:md5, input)
+    hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
-  end 
+
+    %Identicon.Image{hex: hex}
+  end
+
+  # first three values can be rgb
+  # Can pattern match in arg list
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    # Changed to a tuple since each has meaning
+    %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  # Color on even numbers, don't color on odd
 end
+  

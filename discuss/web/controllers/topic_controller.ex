acf4af399bb611744b4ea4d34df0641d84eeb6bb
javier <4.1.4.1.done@gmail.com>
2017-05-12 07:12:12 -0700

    topic creation flow completed

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -2,6 +2,13 @@ defmodule Discuss.TopicController do
   use Discuss.Web, :controller
 
   alias Discuss.Topic
+
+  def index(conn, params) do
+    IO.inspect params
+    topics = Repo.all(Topic)
+    render conn, "index.html", topics: topics
+  end
+
   # conn represents both incoming and outgoing requests
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
@@ -9,7 +16,17 @@ defmodule Discuss.TopicController do
     render conn, "new.html", changeset: changeset # Here is where we define custom vars to template
   end
 
-  def create(conn, params) do
-    IO.inspect params
+  def create(conn, %{"topic" => topic}) do
+    changeset = Topic.changeset(%Topic{}, topic)
+
+    case Repo.insert(changeset) do
+      {:ok, post} ->
+        conn
+        |> put_flash(:info, "Topic Created")
+        |> redirect(to: topic_path(conn, :index))
+        render conn, "index.html", flash: %{message: "Created!"}
+      {:error, changeset} ->
+        render conn, "new.html", changeset: changeset
+    end
   end
 end
